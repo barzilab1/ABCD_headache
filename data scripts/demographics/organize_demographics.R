a5ab7cf1f93ea9be082977f313844dc6b8caf66b
trant13@chop.edu <trant13@chop.edu>
2023-07-05 13:08:34 -0400

    Fix age and household income for wide data

--- data scripts/demographics/organize_demographics.R
+++ data scripts/demographics/organize_demographics.R
@@ -11,6 +11,9 @@ demographics_baseline = demographics_baseline[, grep("race|hisp|born_in_usa", co
 demographics_long = demographics_long[demographics_long$eventname != "baseline_year_1_arm_1",]
 demographics_long = bind_rows(demographics_baseline, demographics_long)
 
+# Use this data for table 1 as in data_long (in merge.R), missing data of headache was removed and it causes a lot of missing data for household income in wide data
+demo_income_age_2y = demographics_long %>% filter(eventname == "2_year_follow_up_y_arm_1") %>% select(src_subject_id, household_income, interview_age) %>%
+    mutate(age_years = interview_age/12)
 
 # SES was estimated using the income-to-needs ratio (INR).
 # The INR was calculated by dividing reported household income by the federal poverty threshold for a given household size.
@@ -76,6 +79,7 @@ demographics_long = bind_rows(demographics_baseline, demographics_long)
 
 write.csv(file = "data/demographics_all.csv", x = demographics_long, row.names=F, na = "")
 write.csv(file = "data/demo_race.csv", x = demo_race, row.names=F, na = "")
+write.csv(file = "data/demo_income_age_2y.csv", x = demo_income_age_2y, row.names=F, na = "")
 
 
 
